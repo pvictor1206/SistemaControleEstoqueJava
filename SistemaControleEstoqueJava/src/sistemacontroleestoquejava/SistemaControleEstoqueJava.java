9534f63e4c2c143f27e5ee9f6866dcaff3a28435
Paulo Victor Santos Magalhães <paulomagalhaes1206@gmail.com>
2022-06-17 20:00:50 -0300

    commit

---
+++ SistemaControleEstoqueJava/src/sistemacontroleestoquejava/SistemaControleEstoqueJava.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sistemacontroleestoquejava;
+
+import com.br.autenticacao.LoginPage;
+import com.br.produto.CadastrarProduto;
+
+/**
+ *
+ * @author paulo
+ */
+public class SistemaControleEstoqueJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new LoginPage().setVisible(true);
+        System.out.println("testeee1123");
+    }
+    
+}
