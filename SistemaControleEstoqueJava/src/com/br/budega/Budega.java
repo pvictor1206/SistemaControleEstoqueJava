f0652f51fe7cb276b5ca3b2a2b0255d7baef6f38
Paulo Victor Santos Magalhães <paulomagalhaes1206@gmail.com>
2022-06-25 17:26:55 -0300

    commit

---
+++ SistemaControleEstoqueJava/src/com/br/budega/Budega.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.br.budega;
+
+import com.br.estoque.IEstoque;
+import com.br.produtos.Produto;
+
+/**
+ *
+ * @author paulo
+ */
+public class Budega {
+    private IEstoque estoque;
+    
+    public Budega(IEstoque estoque) {
+        this.estoque = estoque;
+        
+    }
+
+    public IEstoque getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(IEstoque estoque) {
+        this.estoque = estoque;
+    }
+    
+    
+    
+    public void adicionarProduto(Produto produto){
+        
+    }
+    
+    public void removerProduto(Produto produto){
+        
+    }
+    
+    public void venderProduto(String codigo, int quantidade){
+        
+    }
+    
+    public void estocarProduto(String codigo, int quantidade){
+        
+    }
+    
+    public Produto consultarProduto(String codigo){
+        return null;
+        
+    }
+    
+    public Produto listarProduto(){
+        return null;
+    }
+    
+    public Produto produtosEmFalta(){
+        return null;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ SistemaControleEstoqueJava/src/com/br/estoque/IEstoque.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.br.estoque;
+
+import com.br.produtos.Produto;
+
+/**
+ *
+ * @author paulo
+ */
+public interface IEstoque {
+    
+    public void adicionar(Produto produto);
+    public Produto buscar(String Codigo);
+    public void apagar(Produto produto);
+    public int quantidade();
+    public Produto produtosEmEsquete();
+    public Produto produtoEmFalta();
+}
